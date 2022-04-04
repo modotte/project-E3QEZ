b78f03fd5e53659a7f7f4fc1728053b1cba1356c
modotte <runecroon@gmail.com>
2022-04-04 21:39:35 +0800

    Add basic models

--- src/Main.elm
+++ src/Main.elm
@@ -4,15 +4,46 @@ import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
+import Set exposing (Set)
+
+
+type FirstName
+    = FirstName String
+
+
+type LastName
+    = LastName String
+
+
+type Age
+    = Age Int
+
+
+type alias Player =
+    { firstName : FirstName
+    , lastName : LastName
+    , age : Age
+    }
+
+
+type MusicVolume
+    = MusicVolume Int
+
+
+type alias Settings =
+    { musicVolume : MusicVolume
+    }
 
 
 type alias Model =
-    { value : Int
+    { player : Maybe Player
+    , settings : Settings
     }
 
 
 type Msg
-    = Increment
+    = OnAboutClicked
+    | OnError String
 
 
 main : Program () Model Msg
@@ -27,19 +58,35 @@ main =
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( { value = 0 }, Cmd.none )
+    ( { player = Nothing
+      , settings = { musicVolume = MusicVolume 50 }
+      }
+    , Cmd.none
+    )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Increment ->
-            ( { model | value = model.value + 1 }, Cmd.none )
+        OnError err ->
+            Debug.todo "Not implemented"
+
+        OnAboutClicked ->
+            Debug.todo "Not implemented"
+
+
+mainMenuView : Model -> Html Msg
+mainMenuView model =
+    div []
+        [ h1 [] [ text "Hearties!" ]
+        , hr [] []
+        , button [] [ text "Start Game" ]
+        , button [] [ text "Settings" ]
+        , button [ onClick OnAboutClicked ] [ text "About" ]
+        ]
 
 
 view : Model -> Html Msg
 view model =
     div []
-        [ h1 [] [ text (String.fromInt model.value) ]
-        , button [ onClick Increment ] [ text "+1" ]
-        ]
+        [ mainMenuView model ]
